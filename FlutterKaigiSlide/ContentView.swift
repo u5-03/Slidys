3b56204fb1558cbf132a93e744807ae035b4db74
yugo.sugiyama <sugiy.u.2203@gmail.com>
2024-10-27 16:29:25 +0900

    Initial Commit

---
+++ FlutterKaigiSlide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlutterKaigiSlide
+//
+//  Created by Yugo Sugiyama on 2024/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlutterKaigiSlide/FlutterKaigiSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlutterKaigiSlideApp.swift
+//  FlutterKaigiSlide
+//
+//  Created by Yugo Sugiyama on 2024/10/27.
+//
+
+import SwiftUI
+
+@main
+struct FlutterKaigiSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
