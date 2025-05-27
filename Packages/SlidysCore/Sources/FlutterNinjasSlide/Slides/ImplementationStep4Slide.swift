5026244b405d9da322d8d87849b2fde2f7294ef0
Yugo Sugiyama <yugo.a.sugiyama+dena@dena.com>
2025-05-27 09:31:27 +0900

    Update

--- Packages/SlidysCore/Sources/FlutterNinjasSlide/Slides/ImplementationStep4Slide.swift
+++ Packages/SlidysCore/Sources/FlutterNinjasSlide/Slides/ImplementationStep4Slide.swift
@@ -14,10 +14,9 @@ struct ImplementationStep4Slide: View {
     var body: some View {
         HeaderSlide("Step 4: Enable animation according to progress") {
             Item("Prepare an AnimationController and animation progress value", accessory: .number(1))
-            Item("I used useAnimationController from flutter_hooks for the AnimationController ", accessory: .number(2))
-            Item("Dynamically determine the drawing range of the Path according to the progress value (0.0-1.0)", accessory: .number(3))
-            Item("Execute forward() or repeat() of the AnimationController to run the animation", accessory: .number(4))
-            Item("The Path is gradually drawn as the animation progresses", accessory: .number(5))
+            Item("Set the animation duration to AnimationController", accessory: .number(2))
+            Item("Execute forward() or repeat() of the AnimationController to run the animation according to the progress value (0.0-1.0)", accessory: .number(3))
+            Item("The Path is gradually drawn as the animation progresses", accessory: .number(4))
         }
     }
 }
